e7fd7a39e3bd18d775b496064538123e8345c369
lzhuk <rzdcargo@gmail.com>
2024-04-09 16:42:03 +0600

    Обработан хэндлер моих постов

--- internal/controller/myPosts.go
+++ internal/controller/myPosts.go
@@ -28,36 +28,75 @@ func myPosts(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Произошла ошибка создания шаблона страницы постов пользователя. Ошибка: %v", err)
 		return
 	}
-	// Создаем новый запрос на сервис сервера для получения всех постов пользователя
-	req, err := http.NewRequest("GET", userPost, nil)
-	if err != nil {
-		errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
-		log.Printf("Произошла ошибка при создании запроса для получения с сервера постов пользователя. Ошибка: %v", err)
+	switch r.Method {
+	case http.MethodGet:
+		// Создаем новый запрос на сервис сервера для получения всех постов пользователя
+		req, err := http.NewRequest("GET", userPost, nil)
+		if err != nil {
+			errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
+			log.Printf("Произошла ошибка при создании запроса для получения с сервера постов пользователя. Ошибка: %v", err)
+			return
+		}
+		// Добавление из браузера куки в запрос на сервер
+		req.AddCookie(r.Cookies()[0])
+		// Создаем структуру нового клиента
+		client := http.Client{}
+		// Отправляем запрос на сервис сервера
+		resp, err := client.Do(req)
+		if err != nil {
+			errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
+			log.Printf("Произошла ошибка при передаче запроса на сервис сервера о получении постов пользователя. Ошибка: %v", err)
+			return
+		}
+		defer resp.Body.Close()
+		switch resp.StatusCode {
+		case http.StatusOK:
+			// Конвертация данных о всех постах
+			userPosts, err := convertor.ConvertAllPosts(resp)
+			if err != nil {
+				errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
+				log.Printf("Произошла ошибка при конвертации данных обо всех постах пользователя. Ошибка: %v", err)
+				return
+			}
+			// Рендеринг страницы со всеми постами пользователя
+			err = t.ExecuteTemplate(w, "user_posts.html", userPosts)
+			if err != nil {
+				errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
+				log.Printf("Произошла ошибка при рендеринге страницы со всеми постами пользователя. Ошибка: %v", err)
+				return
+			}
+		case http.StatusInternalServerError:
+			discriptionMsg, err := convertor.DecodeErrorResponse(resp)
+			if err != nil {
+				errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
+				log.Printf("Произошла ошибка при декодировании ответа ошибки и описания от сервера на запрос об получении всех постов пользователя")
+				return
+			}
+			switch {
+			// Получена ошибка что почта уже используется
+			case discriptionMsg.Discription == "Email already exist":
+				errorPage(w, errors.EmailAlreadyExists, http.StatusConflict)
+				log.Printf("Пользователь пытается зарегестировать почту которая используется под другим аккаунтом")
+				return
+				// Получена ошибка что введены неверные учетные данные
+			case discriptionMsg.Discription == "Invalid Credentials":
+				errorPage(w, errors.InvalidCredentials, http.StatusBadRequest)
+				log.Printf("Не валидные данные")
+				return
+			case discriptionMsg.Discription == "Not Found Any Data":
+				errorPage(w, errors.NotFoundAnyDate, http.StatusBadRequest)
+				log.Printf("Не найдено")
+				return
+			default:
+				errorPage(w, errors.ErrorNotMethod, http.StatusMethodNotAllowed)
+				log.Printf("Получена ошибка сервера от сервиса сервера при передаче запроса на получение всех постов пользователя")
+				return
+			}
+		}
+		// Метод запроса с браузера не GET
+	default:
+		errorPage(w, errors.ErrorNotMethod, http.StatusMethodNotAllowed)
+		log.Printf("При передаче запроса на домашнюю страницу не верный метод запроса")
 		return
-	}
-	// Добавление из браузера куки в запрос на сервер
-	req.AddCookie(r.Cookies()[0])
-	// Создаем структуру нового клиента
-	client := http.Client{}
-	
-	resp, err := client.Do(req)
-	if err != nil {
-		http.Error(w, "Request client registry error", http.StatusInternalServerError)
-		return
-	}
-	userPosts, err := convertor.ConvertAllPosts(resp)
-	if err != nil {
-		http.Error(w, "error", http.StatusInternalServerError)
-		return
-	}
-
-	err = t.ExecuteTemplate(w, "user_posts.html", userPosts)
-	if err != nil {
-		http.Error(w, "Error executing template", http.StatusInternalServerError)
-		return
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode == http.StatusOK {
-		http.Redirect(w, r, "http://localhost:8082/userd3", 300)
 	}
 }
