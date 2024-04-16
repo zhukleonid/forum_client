5202d47b5826d4ad9d7523c9d12165a1de846b86
lzhuk <rzdcargo@gmail.com>
2024-04-16 17:03:31 +0600

    Обработаны ошибки обработчика изменения поста

--- internal/controller/updatePost.go
+++ internal/controller/updatePost.go
@@ -40,7 +40,7 @@ func updatePost(w http.ResponseWriter, r *http.Request) {
 		req, err := http.NewRequest("GET", getUserPostId, nil)
 		if err != nil {
 			errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
-			log.Printf("Произошла ошибка при создании запроса на сервер для изменения поста. Ошибка: %v", err)
+			log.Printf("Произошла ошибка при создании GET запроса на сервис forum-api для изменения поста. Ошибка: %v", err)
 			return
 		}
 		// Записываем куки из браузера в запрос к серверу
@@ -51,7 +51,7 @@ func updatePost(w http.ResponseWriter, r *http.Request) {
 		resp, err := client.Do(req)
 		if err != nil {
 			errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
-			log.Printf("Произошла ошибка при передаче запроса на сервер для изменения поста. Ошибка: %v", err)
+			log.Printf("Произошла ошибка при передаче запроса на сервис forum-api для изменения поста. Ошибка: %v", err)
 			return
 		}
 		defer resp.Body.Close()
@@ -62,47 +62,47 @@ func updatePost(w http.ResponseWriter, r *http.Request) {
 			result, err := convertor.ConvertGetPosts(r, resp)
 			if err != nil {
 				errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
-				log.Printf("Произошла ошибка при конвертации данных о посте из БД который пользователь будет редактировать. Ошибка: %v", err)
+				log.Printf("Произошла ошибка при конвертации данных из ответа сервиса forum-api который пользователь будет редактировать. Ошибка: %v", err)
 				return
 			}
 
 			err = t.ExecuteTemplate(w, "update_post.html", result)
 			if err != nil {
 				errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
-				log.Printf("Произошла ошибка при парсинге страницы с данными о посте который пользователь будет редактировать. Ошибка: %v", err)
+				log.Printf("Произошла ошибка при рендеринге страницы с данными о посте который пользователь будет редактировать. Ошибка: %v", err)
 				return
 			}
 		case http.StatusInternalServerError:
 			discriptionMsg, err := convertor.DecodeErrorResponse(resp)
 			if err != nil {
 				errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
-				log.Printf("Произошла ошибка при декодировании ответа ошибки и описания от сервера на запрос об данных о посте который пользователь будет редактировать")
+				log.Printf("Произошла ошибка при декодировании ответа ошибки и её описания от сервиса forum-api на запрос об данных о посте который пользователь будет редактировать")
 				return
 			}
 			switch {
 			// Получена ошибка что почта уже используется
 			case discriptionMsg.Discription == "Email already exist":
 				errorPage(w, errors.EmailAlreadyExists, http.StatusConflict)
-				log.Printf("Пользователь пытается зарегестировать почту которая используется под другим аккаунтом")
+				log.Printf("Не используется при изменении поста")
 				return
 				// Получена ошибка что введены неверные учетные данные
 			case discriptionMsg.Discription == "Invalid Credentials":
 				errorPage(w, errors.InvalidCredentials, http.StatusBadRequest)
-				log.Printf("Не валидные данные")
+				log.Printf("Не валидные данные при изменении поста")
 				return
 			case discriptionMsg.Discription == "Not Found Any Data":
 				errorPage(w, errors.NotFoundAnyDate, http.StatusBadRequest)
-				log.Printf("Не найдено")
+				log.Printf("Нет запрашиваемых данных при изменении поста")
 				return
 			default:
-				errorPage(w, errors.ErrorNotMethod, http.StatusInternalServerError)
-				log.Printf("Получена ошибка сервера от сервиса сервера при передаче запроса на получение старницы с данными о посте который пользователь собирается редактировать")
-				return
+				errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
+					log.Printf("Получена не кастомная ошибка от сервиса forum-api при изменении поста")
+					return
 			}
 		default:
-			errorPage(w, errors.ErrorNotMethod, http.StatusMethodNotAllowed)
-			log.Printf("Получена ошибка сервера от сервиса сервера при передаче запроса на получение старницы с данными о посте который пользователь собирается редактировать")
-			return
+			errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
+				log.Printf("Получен статус-код не 200 и 500 от сервиса forum-api при изменении поста")
+				return
 		}
 	case http.MethodPost:
 		// Проверка на валидность пользовательских данных
@@ -115,7 +115,7 @@ func updatePost(w http.ResponseWriter, r *http.Request) {
 			jsonData, err := convertor.ConvertUpdatePost(r)
 			if err != nil {
 				errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
-				log.Printf("Произошла ошибка при конвертации данных c изменениями внесенными пользователем о посте для передачи на сервер. Ошибка: %v", err)
+				log.Printf("Произошла ошибка при конвертации данных c изменениями внесенными пользователем о посте для передачи на сервис forum-api. Ошибка: %v", err)
 				return
 			}
 			// Формирование URL  c конкретным id поста для редактирования
@@ -124,7 +124,7 @@ func updatePost(w http.ResponseWriter, r *http.Request) {
 			req, err := http.NewRequest("PUT", updatePostsId, bytes.NewBuffer(jsonData))
 			if err != nil {
 				errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
-				log.Printf("Произошла ошибка при подготовке запроса об изменениях данных в посте. Ошибка: %v", err)
+				log.Printf("Произошла ошибка при формировании PUT запроса об изменениях данных в посте. Ошибка: %v", err)
 				return
 			}
 			// Записываем куки из браузера в запрос на сервер
@@ -137,7 +137,7 @@ func updatePost(w http.ResponseWriter, r *http.Request) {
 			resp, err := client.Do(req)
 			if err != nil {
 				errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
-				log.Printf("Произошла ошибка при передаче запроса на сервис сервера об изменениях данных в посте. Ошибка: %v", err)
+				log.Printf("Произошла ошибка при передаче запроса на сервис forum-api об изменениях данных в посте. Ошибка: %v", err)
 				return
 			}
 			defer resp.Body.Close()
@@ -148,7 +148,7 @@ func updatePost(w http.ResponseWriter, r *http.Request) {
 				newReq, err := http.NewRequest("GET", fmt.Sprintf("http://localhost:8082/userd3/post/%s", r.FormValue("postId")), nil)
 				if err != nil {
 					errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
-					log.Printf("Произошла ошибка при передаче запроса на сервис сервера об изменениях данных в посте. Ошибка: %v", err)
+					log.Printf("Произошла ошибка при формировании GET запроса на сервис forum-api об изменениях данных в посте. Ошибка: %v", err)
 					return
 				}
 				http.Redirect(w, r, newReq.URL.String(), 302)
@@ -156,39 +156,38 @@ func updatePost(w http.ResponseWriter, r *http.Request) {
 				discriptionMsg, err := convertor.DecodeErrorResponse(resp)
 				if err != nil {
 					errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
-					log.Printf("Произошла ошибка при декодировании ответа ошибки и описания от сервера на запрос об данных о посте который пользователь будет редактировать")
+					log.Printf("Произошла ошибка при декодировании ответа ошибки и её описания от сервиса forum-api на запрос об данных о посте который пользователь будет редактировать")
 					return
 				}
 				switch {
 				// Получена ошибка что почта уже используется
 				case discriptionMsg.Discription == "Email already exist":
 					errorPage(w, errors.EmailAlreadyExists, http.StatusConflict)
-					log.Printf("Пользователь пытается зарегестировать почту которая используется под другим аккаунтом")
+					log.Printf("Не используется при изменении поста")
 					return
 					// Получена ошибка что введены неверные учетные данные
 				case discriptionMsg.Discription == "Invalid Credentials":
 					errorPage(w, errors.InvalidCredentials, http.StatusBadRequest)
-					log.Printf("Не валидные данные")
+					log.Printf("Не валидные данные при изменении поста")
 					return
 				case discriptionMsg.Discription == "Not Found Any Data":
 					errorPage(w, errors.NotFoundAnyDate, http.StatusBadRequest)
-					log.Printf("Не найдено")
+					log.Printf("Нет запрашиваемых данных при изменении поста")
 					return
 				default:
 					errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
-					log.Printf("Получена ошибка сервера от сервиса сервера при передаче запроса на получение старницы с данными о посте который пользователь собирается редактировать")
-					return
+						log.Printf("Получена не кастомная ошибка от сервиса forum-api при внесении изменений поста")
+						return
 				}
 			default:
 				errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
-				log.Printf("Получена ошибка сервера от сервиса сервера при передаче запроса на получение старницы с данными о посте который пользователь собирается редактировать")
+				log.Printf("Получен статус-код не 202 и 500 от сервиса forum-api при внесении изменений поста")
 				return
-
 			}
 		}
 	default:
 		errorPage(w, errors.ErrorNotMethod, http.StatusMethodNotAllowed)
-		log.Printf("Не верный метод запроса при изменении данных поста")
+		log.Printf("При передаче запроса сервису forum-client на изменение поста используется не верный метод")
 		return
 	}
 }
