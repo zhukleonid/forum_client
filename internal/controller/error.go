c65c24dc27cdf0f73450dd6ee33c2b9ebb15adea
lzhuk <rzdcargo@gmail.com>
2024-04-17 10:24:07 +0600

    Обработаны ошибки обработчика ошибок

--- internal/controller/error.go
+++ internal/controller/error.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -19,11 +20,13 @@ func errorPage(w http.ResponseWriter, statusText string, statusCode int) {
 
 	t, err := template.ParseFiles("./ui/html/error.html")
 	if err != nil {
-		http.Error(w, "Error parsing template", http.StatusInternalServerError)
+		http.Error(w, "Произошла ошибка на сервере, зайдите на форум позже", http.StatusInternalServerError)
+		log.Printf("Произошла ошибка создании шаблона страницы ошибок. Ошибка: %v", err)
 		return
 	}
 	err = t.ExecuteTemplate(w, "error.html", data)
 	if err != nil {
+		log.Printf("Произошла ошибка при рендеринге страницы ошибок. Ошибка: %v", err)
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusInternalServerError)
 		return
 	}
