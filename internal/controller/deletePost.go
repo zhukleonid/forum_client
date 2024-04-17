465c4d8b98546b6832f5f2e3da90e6deddf45f61
lzhuk <rzdcargo@gmail.com>
2024-04-17 10:20:57 +0600

    Обработаны ошибки обработчика удаления постов

--- internal/controller/deletePost.go
+++ internal/controller/deletePost.go
@@ -27,7 +27,7 @@ func deletePost(w http.ResponseWriter, r *http.Request) {
 		req, err := http.NewRequest("DELETE", fmt.Sprintf(deletePosts+"%s", r.FormValue("postId")), nil)
 		if err != nil {
 			errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
-			log.Printf("Произошла ошибка при создании запроса на удаление конкретного поста по id. Ошибка: %v", err)
+			log.Printf("Произошла ошибка при формировании DELETE запроса на удаление конкретного поста пользователя. Ошибка: %v", err)
 			return
 		}
 		// Записываем куки из браузера в запрос на сервис сервера
@@ -38,7 +38,7 @@ func deletePost(w http.ResponseWriter, r *http.Request) {
 		resp, err := client.Do(req)
 		if err != nil {
 			errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
-			log.Printf("Произошла ошибка при отправке запроса на удаление конкретного поста по id. Ошибка: %v", err)
+			log.Printf("Произошла ошибка при отправке запроса на сервис forum-api на удаление конкретного поста пользователя. Ошибка: %v", err)
 			return
 		}
 		defer resp.Body.Close()
@@ -50,37 +50,37 @@ func deletePost(w http.ResponseWriter, r *http.Request) {
 			discriptionMsg, err := convertor.DecodeErrorResponse(resp)
 			if err != nil {
 				errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
-				log.Printf("Произошла ошибка при декодировании ответа ошибки и описания от сервера на запрос об данных о посте который пользователь будет редактировать")
+				log.Printf("Произошла ошибка при декодировании ответа ошибки и её описания от сервиса forum-api на запрос об удалении конкретного поста пользователя")
 				return
 			}
 			switch {
 			// Получена ошибка что почта уже используется
 			case discriptionMsg.Discription == "Email already exist":
 				errorPage(w, errors.EmailAlreadyExists, http.StatusConflict)
-				log.Printf("Пользователь пытается зарегестировать почту которая используется под другим аккаунтом")
+				log.Printf("Не используется при удалении конкретного поста пользователя")
 				return
 				// Получена ошибка что введены неверные учетные данные
 			case discriptionMsg.Discription == "Invalid Credentials":
 				errorPage(w, errors.InvalidCredentials, http.StatusBadRequest)
-				log.Printf("Не валидные данные")
+				log.Printf("Не валидные данные при удалении конкретного поста пользователя")
 				return
 			case discriptionMsg.Discription == "Not Found Any Data":
 				errorPage(w, errors.NotFoundAnyDate, http.StatusBadRequest)
-				log.Printf("Не найдено")
+				log.Printf("Не найдены данные об удаляемом посте пользователя")
 				return
 			default:
-				errorPage(w, errors.ErrorNotMethod, http.StatusInternalServerError)
-				log.Printf("Получена ошибка сервера от сервиса сервера при передаче запроса на удаление конкретного поста")
+				errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
+				log.Printf("Получена не кастомная ошибка от сервиса forum-api при удалении конкретного поста пользователя")
 				return
 			}
 		default:
-			errorPage(w, errors.ErrorNotMethod, http.StatusInternalServerError)
-			log.Printf("Получена ошибка сервера от сервиса сервера при передаче запроса на удаление конкретного поста")
+			errorPage(w, errors.ErrorServer, http.StatusInternalServerError)
+			log.Printf("Получен статус-код не 202 или 500 от сервиса forum-api при удалении конкретного поста пользователя")
 			return
 		}
 	default:
 		errorPage(w, errors.ErrorNotMethod, http.StatusMethodNotAllowed)
-		log.Printf("Не верный метод запроса при удалении поста")
+		log.Printf("При передаче запроса сервису forum-client на удаление конкретного поста пользователя используется не верный метод")
 		return
 	}
 }
